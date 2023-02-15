d57253375fe248c2bbbea0ee40192fb616e3d3fb
Biel-Alves <gabriel_vieira2018@hotmail.com>
2023-02-15 10:08:13 +0100

    first commit

---
+++ src/be/intecbrussel/MainApp.java
@@ -0,0 +1,19 @@
+package be.intecbrussel;
+
+public class MainApp {
+//    PascalCase -> MyFirstClass -> alle worden beginnen met een hoofdletters -> benaming van classes.
+//    camelCase -> myFirstMethod -> eerst word met een kleiner letter de rest met een hoofdletter -> benaming van methodes en variabelen
+
+    /*
+    *
+    * Multiple line comment
+    *
+    */
+
+    /**
+     *
+     * javadoc -> mogelijk om een document te genereren.
+     *
+     */
+
+}
